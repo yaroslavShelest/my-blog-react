dbb5c6959a19a7298f3f95970075bd656dd55a4c
Alex <alexxldub@gmail.com>
2020-04-09 21:24:22 +0300

    FewInfoAboutMe correction

--- my-blog-react/src/Components/MainPage/FewInfoAboutMe.jsx
+++ my-blog-react/src/Components/MainPage/FewInfoAboutMe.jsx
@@ -1,44 +1,43 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import Divider from '@material-ui/core/Divider';
+import Divider from "@material-ui/core/Divider";
 import Box from "@material-ui/core/Box";
 import { lighten, makeStyles, withStyles } from "@material-ui/core/styles";
 import Rating from "@material-ui/lab/Rating";
 import Typography from "@material-ui/core/Typography";
 import Slide from "@material-ui/core/Slide";
 import Paper from "@material-ui/core/Paper";
-import image from "./../../Assets/myFotos/Cool.jpg";
+import image from "./../../Assets/myFotos/Cool.png";
 import backAboutMe from "./../../Assets/backgrounds/backAboutMe.png";
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
     // margin: `${theme.spacing(1)}px auto`,
-    padding: theme.spacing(2)
+    padding: theme.spacing(2),
+    background: "inherit",
+    boxShadow: "none",
   },
   mainPhotoContainer: {
     margin: "12px",
     width: "200px",
     maxHeight: "200px",
-    boxSizing: "border-box"
+    boxSizing: "border-box",
   },
   mainTextContainer: {
-    margin: "12px"
-    
+    margin: "12px",
   },
   mainPhoto: {
-    borderRadius: "50%",
-    width: "150px",
-    height: "150px"
+    width: "100%",
+    height: "150px",
   },
   dartPhoto: {
     width: "100%",
-    height: "100%"
+    height: "100%",
   },
   hello: {
-    textAlign: "center" ,
-    margin: "10px 0",
+    textAlign: "center",
+    margin: "12px",
     textTransform: "uppercase",
     letterSpacing: "4px",
     color: "black",
@@ -74,15 +73,15 @@ const useStyles = makeStyles(theme => ({
                                   0px   -2px yellow,
                                -0.5px   -2px yellow,
                                  -1px   -2px yellow,
-                               -1.5px   -2px yellow;`
+                               -1.5px   -2px yellow;`,
   },
   positionContainer: {
     position: "relative",
-    width: "100%", /* for IE 6 */
+    width: "100%" /* for IE 6 */,
   },
   sectionImage: {
     maxHeight: "1200px",
-    width: "100%"
+    width: "100%",
   },
   sectionMovedContent: {
     width: "50%",
@@ -99,39 +98,43 @@ const useStyles = makeStyles(theme => ({
     textTransform: "uppercase",
     letterSpacing: "5px",
   },
-  textBlock:{
+  textBlock: {
+    color: "white",
+  },
+  aboutMeDesc: {
+    font: `bold 34px/55px Helvetica, Sans-Serif`,
     color: "white"
   }
 }));
 
-export default function FewInfoAboutMe({ aboutMeFew, aboutMeDescription, hasFindWork}) {
+export default function FewInfoAboutMe({
+  aboutMeFew,
+  aboutMeDescription,
+  hasFindWork,
+}) {
   const classes = useStyles();
 
   return (
-      <Slide direction="left" in={true}  >
-        <section className={classes.paper} >
-          <Box className={classes.positionContainer}>
-            <img src={backAboutMe} alt={"404"} className={classes.sectionImage} />
-            <Box className={classes.sectionMovedContent} >
-          <Typography variant="h4" className={classes.hello}>{aboutMeFew}</Typography>
-          <Divider />
-          <Grid container direction="row" justify="center"  >
-            <Grid item >
-              <Box className={classes.mainPhotoContainer} >
-                <div className="animation-target" >
-                  <img src={image} alt={"404"} className={classes.mainPhoto} />
-                </div>
-              </Box>
-            </Grid>
-            <Grid item >
-              <Box className={classes.mainTextContainer}>
-                <Typography className={classes.textBlock} variant="h6">{aboutMeDescription}</Typography>
-              </Box>
-            </Grid>
+    <Slide direction="left" in={true}>
+      <Paper className={classes.paper}>
+        <Grid container direction="row-reverse" wrap="nowrap">
+          <Grid item xs={6}>
+          <Box className={classes.mainPhotoContainer}>
+              <img src={image} alt={"404"} className={classes.mainPhoto} />
+          </Box>
           </Grid>
-          </Box>
-          </Box>
-        </section>
-      </Slide>
+          <Grid item className={classes.gridContainer}>
+            <Box >
+              <Typography variant="h4" className={classes.hello}>
+                {aboutMeFew}
+              </Typography>
+            </Box>
+              <Box className={classes.mainTextContainer}>
+                <Typography variant="h6" className={classes.aboutMeDesc}>{aboutMeDescription}</Typography>
+              </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Slide>
   );
 }
